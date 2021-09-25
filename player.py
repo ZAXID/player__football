aa99a8e7de154f6d4b03fb54ddefc90718a687f5
ZAXID <ssvladislav2007@gmail.com>
2021-09-25 18:32:25 +0200

    add player

--- player.py
+++ player.py
@@ -1,91 +1,32 @@
 class Football:
-main()
-class Critter:
-    """Football_player"""
-    total = 0
+    def __init__ (self, name, position):
+        self.name = name
+        self.position = position
 
-    def status():
-        print("number of foodball players", Critter.total)
-
-    def __init__(self, name,):
-        self.__name = name
-        Critter.total += 1
-
-    def __str__(self):
-        ans = 'object of class Critter\n'
-        ans += 'name: ' + self.name + '\n'
-        return ans
-
-    def name(self):
-        print('Тибо', 'Давид', 'Начо', 'Даниэль', 'Мигель',
-              'Эдер', 'Марко', 'Родриго', 'Лука', 'Антонио', 'Карим')
-        return self.__name
-
-
-    def pos(self):
-        print('1', '4', '6', '2', '35', '3', '11', '21', '10', '27', '9')
-
-
-def main1():
-    crit_name = input("How do you want to name your player: ")
-    crit = Critter(crit_name)
+    def talk(self):
+        print("How do you want to name football player", self.name, "What is his position", self.position)
 
 def main():
-    foot = Football()
-    foot.name()
-    foot.pos()
-
-def main2():
-    crit_name = input("How do you want to name your player: ")
-    crit = Critter(crit_name)
-
-main()
-
-def main3():
-    crit_name = input("How do you want to name your player: ")
-    crit = Critter(crit_name)
-
-def main4():
-    crit_name = input("How do you want to name your player: ")
-    crit = Critter(crit_name)
-
-def main5():
-    crit_name = input("How do you want to name your player: ")
-    crit = Critter(crit_name)
-
-def main6():
-    crit_name = input("How do you want to name your player: ")
-    crit = Critter(crit_name)
-
-def main7():
-    crit_name = input("How do you want to name your player: ")
-    crit = Critter(crit_name)
-
-def main8():
-    crit_name = input("How do you want to name your player: ")
-    crit = Critter(crit_name)
-
-def main9():
-    crit_name = input("How do you want to name your player: ")
-    crit = Critter(crit_name)
-
-def main10():
-    crit_name = input("How do you want to name your player: ")
-    crit = Critter(crit_name)
-
-def main11():
-    crit_name = input("How do you want to name your player: ")
-    crit = Critter(crit_name)
-
-
-main1()
-main2()
-main3()
-main4()
-main5()
-main6()
-main7()
-main8()
-main9()
-main10()
-main11()
+    crit1 = Football ("Тибо", "1")
+    crit1.talk()
+    crit2 = Football ("Давид", "4")
+    crit2.talk()
+    crit3 = Football ("Начо", "6")
+    crit3.talk()
+    crit4 = Football ("Даниэль", "2")
+    crit4.talk()
+    crit5 = Football ("Мигель", "35")
+    crit5.talk()
+    crit6 = Football ("Эдер", "3")
+    crit6.talk()
+    crit7 = Football ("Марко", "11")
+    crit7.talk()
+    crit8 = Football ("Родриго", "21")
+    crit8.talk()
+    crit9 = Football ("Лука", "10")
+    crit9.talk()
+    crit10 = Football ("Антонио", "27")
+    crit10.talk()
+    crit11 = Football ("Карим", "9")
+    crit11.talk()
+main() 
