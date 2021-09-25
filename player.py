f59cdbece690235c15b511425c7449419b3e6604
ZAXID <ssvladislav2007@gmail.com>
2021-09-25 17:40:11 +0200

    add player

---
+++ player.py
@@ -0,0 +1,10 @@
+class Football:
+    def name(self):
+        print('Тибо', 'Давид', 'Начо', 'Даниэль', 'Мигель', 'Эдер', 'Марко', 'Родриго', 'Лука', 'Антонио', 'Карим')
+    def pos(self):
+        print('1', '4', '6', '2', '35', '3', '11', '21', '10', '27', '9',)
+def main():
+    foot=Football()
+    foot.name()
+    foot.pos()
+main()
