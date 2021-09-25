ceaa1a9e50e898135ace765b1fa122eb4c37a50f
ZAXID <ssvladislav2007@gmail.com>
2021-09-25 17:49:51 +0200

    add player

--- player.py
+++ player.py
@@ -1,8 +1,58 @@
 class Football:
     def name(self):
-        print('Тибо', 'Давид', 'Начо', 'Даниэль', 'Мигель', 'Эдер', 'Марко', 'Родриго', 'Лука', 'Антонио', 'Карим')
+        print('Тибо')
     def pos(self):
-        print('1', '4', '6', '2', '35', '3', '11', '21', '10', '27', '9',)
+        print('1')
+
+    def name(self):
+        print('Давид')
+    def pos(self):
+        print('4')
+
+    def name(self):
+        print('Начо')
+    def pos(self):
+        print('6')
+
+    def name(self):
+        print('Даниэль')
+    def pos(self):
+        print('2')
+
+    def name(self):
+        print('Мигель')
+    def pos(self):
+        print('35')
+
+    def name(self):
+        print('Эдер')
+    def pos(self):
+        print('3')
+
+    def name(self):
+        print('Марко')
+    def pos(self):
+        print('11')
+
+    def name(self):
+        print('Родриго')
+    def pos(self):
+        print('21')
+
+    def name(self):
+        print('Лука')
+    def pos(self):
+        print('10')
+
+    def name(self):
+        print('Антонио')
+    def pos(self):
+        print('27')
+
+    def name(self):
+        print('Карим')
+    def pos(self):
+        print('9')
 def main():
     foot=Football()
     foot.name()
